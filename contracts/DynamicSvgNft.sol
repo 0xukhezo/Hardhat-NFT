07ce537d085e94627b64661f10c4711ff381302f
0xukhezo <ukhezo.web3@gmail.com>
2022-11-25 12:34:15 +0100

    Finish DynamicSVGNft functions

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 
 contract DinamicSvgNft is ERC721 {
@@ -10,12 +11,18 @@ contract DinamicSvgNft is ERC721 {
     string private i_imageHighSvg;
     string private constant base64EncondedSvgPrefix =
         "data:image/svg+xml;base64,";
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) public s_tokenIdToHighValue;
 
     constructor(
-        string memory _lowSvg,
-        string memory _highSvg
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
     ) ERC721("Dynamic Svg Nft", "DSN") {
         s_tokenCounter = 0;
+        i_imageLowSvg = svgToImageURI(lowSvg);
+        i_imageHighSvg = svgToImageURI(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function svgToImageURI(
@@ -28,7 +35,8 @@ contract DinamicSvgNft is ERC721 {
             string(abi.encodePacked(base64EncondedSvgPrefix, svgBase64Encoded));
     }
 
-    function mintNft() public {
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValue[s_tokenCounter] = highValue;
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
     }
@@ -41,8 +49,13 @@ contract DinamicSvgNft is ERC721 {
         uint256 tokenId
     ) public view override returns (string memory) {
         require(_exists(tokenId), "URI Query for nonexistent token");
-        string memory imageURI = "hi!";
+        // string memory imageURI = "hi!";
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = i_imageLowSvg;
 
+        if (price >= s_tokenIdToHighValue[tokenId]) {
+            imageURI = i_imageHighSvg;
+        }
         return
             string(
                 abi.encodePacked(
